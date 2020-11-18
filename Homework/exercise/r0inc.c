8a1e801920cb385209f3c0ef6c4578166436a88c
WWW-Jack <jack07528@gmail.com>
2020-11-19 00:43:37 +0800

    add homework8

--- Homework/exercise/r0inc.c
+++ Homework/exercise/r0inc.c
@@ -10,14 +10,11 @@ while(1)
 void main()
 {
     int R0 = 1;
-    //while(1)
-    //{
     LOOP:
         R0 = R0 + 1;
         if(R0 == 10) 
             goto EXIT;
         printf("R0 = %d\n", R0);
         goto LOOP;
-    //}
     EXIT:
 }
