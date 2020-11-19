817983f0f9a0026bc4d4bc1c3d9b3e36ae3ed436
WWW-Jack <jack07528@gmail.com>
2020-11-19 10:12:49 +0800

    add homework8era1

--- Homework/exercise/r0inc.c
+++ Homework/exercise/r0inc.c
@@ -12,9 +12,6 @@ void main()
     int R0 = 1;
     LOOP:
         R0 = R0 + 1;
-        if(R0 == 10) 
-            goto EXIT;
-        printf("R0 = %d\n", R0);
+        print("R0 = %d\n", R0);
         goto LOOP;
-    EXIT:
 }
